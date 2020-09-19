4c30e02625245f5e8f58f29201014ba4b88a2161
federico-hausler <federicohausler@gmail.com>
2020-09-19 15:48:38 -0300

    Commented stuff

--- ruby/lib/main.rb
+++ ruby/lib/main.rb
@@ -4,20 +4,32 @@ module MethodInterceptors
   def self.included(klass)
     klass.class_eval do
       def self.method_added(method_name)
-        puts method_name
+        puts method_name # TODO: Here we need to grab and modify every method definition with before and after
       end
     end
   end
 
-  def before_and_after_each_call(before, after)
-    puts 'Hello'
+  def call_procs_after
+    self.class.singleton_class.instance_variable_get(:@after_list).each(&:call)
   end
 
+  def call_procs_before
+    self.class.singleton_class.instance_variable_get(:@before_list).each(&:call)
+  end
 
+  # def before_and_after_each_call(before, after)
+  #   singleton_class.instance_variable_set(:@before_list, []) if singleton_class.instance_variable_get(:@before_list).nil? # TODO: Can we access directly to variables?
+  #   singleton_class.instance_variable_set(:@after_list, []) if singleton_class.instance_variable_get(:@after_list).nil?
+  #
+  #   singleton_class.instance_variable_get(:@before_list) << before
+  #   singleton_class.instance_variable_get(:@after_list) << after
+  # end
 end
 
 class Cat
   include MethodInterceptors
+
+  # before_and_after_each_call(proc { puts 'before' }, proc { puts 'after' })
 
   def hello_world(name)
     puts "Hello #{name}"
