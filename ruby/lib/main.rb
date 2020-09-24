bf68691d4d1d6ca77f7f3d19d528628c0065fbf8
Gonzalo Bertinat <gonzalo.bertinat@snoopconsulting.com>
2020-09-24 20:34:45 -0300

    Fix para que se ejecuten los procs en cada clase y no globalmente

--- ruby/lib/main.rb
+++ ruby/lib/main.rb
@@ -2,18 +2,18 @@ require 'set'
 
 module MethodInterceptors
   def llamar_before_procs
-    @@before_list.each { |bloque| bloque.call }
+    @before_list.each { |bloque| bloque.call }
   end
 
   def llamar_after_procs
-    @@after_list.each { |bloque| bloque.call }
+    @after_list.each { |bloque| bloque.call }
   end
 
   def method_added(method_name)
     @@recursing = true
     define_aleady_intercepted_methods
     if method_name != :method_added && not_intercepted(method_name)
-      @@already_intercepted_methods << method_name
+      @already_intercepted_methods << method_name
       unbound_method = self.instance_method(method_name)
       los_parametros = unbound_method.parameters[0]
       if not_nil_and_not_empty(los_parametros)
@@ -44,21 +44,21 @@ module MethodInterceptors
     define_before_list_if_not_defined
     define_after_list_if_not_defined
 
-    @@before_list << before
-    @@after_list << after
+    @before_list << before
+    @after_list << after
   end
 
   private
 
   def not_intercepted(method_name)
-    !@@already_intercepted_methods.include?(method_name)
+    !@already_intercepted_methods.include?(method_name)
   end
 
   def define_aleady_intercepted_methods
-    isNotDefined = (defined? @@already_intercepted_methods).nil?
+    isNotDefined = (defined? @already_intercepted_methods).nil?
 
     if isNotDefined
-      @@already_intercepted_methods = Set[]
+      @already_intercepted_methods = Set[]
     end
   end
 
@@ -67,18 +67,18 @@ module MethodInterceptors
   end
 
   def define_after_list_if_not_defined
-    isNotDefined = (defined? @@after_list).nil?
+    isNotDefined = (defined? @after_list).nil?
 
     if isNotDefined
-      @@after_list = []
+      @after_list = []
     end
   end
 
   def define_before_list_if_not_defined
-    isNotDefined = (defined? @@before_list).nil?
+    isNotDefined = (defined? @before_list).nil?
 
     if isNotDefined
-      @@before_list = []
+      @before_list = []
     end
   end
 end
@@ -88,8 +88,7 @@ class Class
 end
 
 class Cat
-  before_and_after_each_call(proc { puts 'before' }, proc { puts 'after' })
-  before_and_after_each_call(proc { puts 'before2' }, proc { puts 'after2' })
+  before_and_after_each_call(proc { puts 'BEFORE_DE_CAT' }, proc { puts 'AFTER_DE_CAT' })
 
   def hello_world
     puts "Hello"
@@ -100,10 +99,27 @@ class Cat
   end
 end
 
+class Pepito
+  before_and_after_each_call(proc do puts 'Este es el BEFORE DE PEPITO'
+  hola = 3
+  puts hola end, proc { puts 'Este es el AFTER DE PEPITO' })
+
+  def hola
+    puts "EJECUTANDO METODO HOLA"
+  end
+end
+
+class ClaseSinBeforeAndAfter
+  def chau
+    puts "EJECUTANDO METODO CHAU"
+  end
+end
+
 my_cat = Cat.new
 my_cat.hello_world
+puts "==========================="
 my_cat.hello_world_con_nombre("Paul")
-
-
-
-
+puts "==========================="
+Pepito.new.hola
+puts "==========================="
+ClaseSinBeforeAndAfter.new.chau
